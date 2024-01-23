eb1fc826165c0e87d8ee05fbbec10701fcacd8b9
Maurice Herzog <mauriceder4te@gmail.com>
2024-01-23 08:44:52 +0100

    Projektdateien hinzufügen.

---
+++ Flappy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlapFlap
+{
+    internal class Flappy
+    {
+
+    }
+    public class Board
+    {
+        public int Hight { set; get; }
+        public int Width { set; get; }
+
+        public Board()
+        {
+            Hight = 20;
+            Width = 80;
+        }
+
+        public Board(int hight, int width)
+        {
+            Hight = hight;
+            Width = width;
+        }
+        public void Write ()
+        {
+            for (int i = 1;i <= Width;i++) 
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("─");
+            }
+            for (int i = 1; i <= Width; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("─");
+            }
+            for (int i = 1; i <= Width; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("─");
+            }
+            for (int i = 1; i <= Width; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("│");
+            }
+            Console.SetCursorPosition(0, 0);
+            Console.Write("┌");
+            Console.SetCursorPosition(Width+1, 0);
+            Console.Write("┐");
+            Console.SetCursorPosition(0, Hight+1);
+            Console.Write("└");
+            Console.SetCursorPosition(Width +1, Hight+1);
+            Console.Write("┘");
+        }
+    }
+}

c39abe11696affd272d6e5902c7a569410dea0f2
Maurice Herzog <mauriceder4te@gmail.com>
2024-01-23 09:31:54 +0100

    langsame verzweiflung

--- Flappy.cs
+++ Flappy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,65 @@ namespace FlapFlap
             Console.Clear();
             Setup();
             board.Write();
+        }
+    }
+    class Bird
+    {
+        public int X { set; get; }
+        public int Y { set; get; }
+        ConsoleKeyInfo keyInfo;
+        ConsoleKey consoleKey;
+
+        public Bird (int x, int y)
+        {
+            X=x;
+            Y=y;
+            keyInfo = new ConsoleKeyInfo();
+            consoleKey = new ConsoleKey();
+        }
+        void Input ()
+        {
+            if (Console.KeyAvailable)
+            {
+                keyInfo = Console.ReadKey(true);
+                consoleKey = keyInfo.Key;
+            }
+        }
+        public void Logic ()
+        {
+
+        }
+        void Up()
+        {
+            Y--;
+            Write();
+        }
+
+        public void Write()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.SetCursorPosition(X,Y-1);
+            Console.Write("▄");
+            Console.SetCursorPosition(X-1,Y-1);
+            Console.Write("▄");
+            Console.SetCursorPosition(X-1,Y);
+            Console.Write("▀");
+            Console.SetCursorPosition(X,Y);
+            Console.Write("▀");
+            Console.ForegroundColor= ConsoleColor.White;
+        }
+        public void Write(string str)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.SetCursorPosition(X, Y - 1);
+            Console.Write("▄");
+            Console.SetCursorPosition(X - 1, Y - 1);
+            Console.Write("▄");
+            Console.SetCursorPosition(X - 1, Y);
+            Console.Write("▀");
+            Console.SetCursorPosition(X, Y);
+            Console.Write("▀");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
     public class Board
--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace FlapFlap
         {
             Console.Title = "Console Flappy Bird";
             Console.CursorVisible = false;
-            Flappy flappy = new Flappy(75, 20);
+            Flappy flappy = new Flappy(25,110);
             flappy.Run();
             Console.ReadKey();
         }
